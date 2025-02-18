4eaa9e60e8ba76573c5c5e77be67c6863112c89d
penhele <stephenhelk@gmail.com>
2025-02-18 19:16:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:declarative_navigation/db/auth_repository.dart';
+import 'package:declarative_navigation/provider/auth_provider.dart';
+import 'package:declarative_navigation/routes/router_delegate.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const QuotesApp());
+}
+
+class QuotesApp extends StatefulWidget {
+  const QuotesApp({super.key});
+
+  @override
+  State<QuotesApp> createState() => _QuotesAppState();
+}
+
+class _QuotesAppState extends State<QuotesApp> {
+  late MyRouterDelegate myRouterDelegate;
+
+  late AuthProvider authProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    final authRepository = AuthRepository();
+
+    authProvider = AuthProvider(authRepository);
+
+    myRouterDelegate = MyRouterDelegate(authRepository);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => authProvider,
+      child: MaterialApp(
+        title: 'Quotes App',
+        home: Router(
+          routerDelegate: myRouterDelegate,
+          backButtonDispatcher: RootBackButtonDispatcher(),
+        ),
+      ),
+    );
+  }
+}
